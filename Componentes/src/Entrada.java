d3b4e273274daf19601aaa639b1fbe5be0804253
Joselu02 <josebttmx26@gmail.com>
2026-09-21 17:00:51 +0200

    update

---
+++ Componentes/src/Entrada.java
@@ -0,0 +1,17 @@
+/**
+ * @author joseb
+ * @version 1.0
+ */
+
+public class Entrada {
+    //Comentario de 1 linea
+    //Ctrl D Duplicar linea
+    /*
+    Varias lineas
+     */
+    // TODO Tarea pendiente
+
+ public static void main(String[] args) {}
+    }
+
+
