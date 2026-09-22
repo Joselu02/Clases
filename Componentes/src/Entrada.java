c14058b40fdfe472c9b9b5b04efaf0e406cd23e9
Joselu02 <josebttmx26@gmail.com>
2026-09-22 17:04:32 +0200

    update

--- Componentes/src/Entrada.java
+++ Componentes/src/Entrada.java
@@ -4,17 +4,47 @@
  */
 
 public class Entrada {
-    //Comentario de 1 linea
-    //Ctrl D Duplicar linea
-    /*
-    Varias lineas
-     */
-    // TODO Tarea pendiente
-    //Comentario1
-    //Comentario2
-    //Comentario3
 
- public static void main(String[] args) {}
+ public static void main(String[] args) {
+
+     String nombre = "Jose";
+     char letra = 'X';
+     int edad = 24;
+     double altura = 1.78;
+     boolean acierto = true;
+     double peso = 74.6;
+
+     System.out.println("Hola Mundo");
+     System.out.println("Segunda Linea");
+     System.out.println("\tTercera Linea\n");
+     System.out.println("Cuarta Linea");
+     System.out.println('a');
+     System.out.println(5*3);
+     System.out.println("Mi nombre es "+nombre);
+     System.out.println("La letra es " +letra);
+     System.out.println("Mi edad es "+edad);
+     System.out.println("El resultado es "+acierto);
+     System.out.println("\nEl peso es "+peso);
+
+     //Variables: String, char, byte/short/int/long, double/float, boolean
+
+ }
+
     }
 
 
+
+
+
+
+
+
+//Comentario de 1 linea
+//Ctrl D Duplicar linea
+    /*
+    Varias lineas
+     */
+// TODO Tarea pendiente
+//Comentario1
+//Comentario2
+//Comentario3
